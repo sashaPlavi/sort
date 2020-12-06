ecc00a3142c2c2e0e82453e59c26e8d8322bfa67
SashaPlavi <sasha.bingulac@gmail.com>
2020-12-06 17:01:37 +0100

    linked list working

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './sorter/Sorter';
 import { NumbersCollection } from './sorter/helper/NumbersCollection';
 import { CharactersCollection } from './sorter/helper/CharactersCollection';
+import { LinkedList } from './sorter/helper/LinkedList';
 
 // const numberscollection = new NumbersCollection([10, 28, 3, 4, -5]);
 // const sorter = new Sorter(numberscollection);
@@ -8,9 +9,21 @@ import { CharactersCollection } from './sorter/helper/CharactersCollection';
 // sorter.sort();
 // //console.log(numberscollection.data);
 
-const characterscoll = new CharactersCollection('danka je danka');
-const sorter = new Sorter(characterscoll);
+// const characterscoll = new CharactersCollection('danka je danka');
+// const sorter = new Sorter(characterscoll);
 
+// sorter.sort();
+
+//console.log(characterscoll.data);
+
+const linkedlist = new LinkedList();
+
+linkedlist.add(500);
+linkedlist.add(50);
+linkedlist.add(-50);
+linkedlist.add(5);
+linkedlist.add(-500);
+
+const sorter = new Sorter(linkedlist);
 sorter.sort();
-
-console.log(characterscoll.data);
+linkedlist.print();
