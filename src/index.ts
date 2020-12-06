77d60306c14cf75d94b91ef55faaed686253caa0
SashaPlavi <sasha.bingulac@gmail.com>
2020-12-06 17:43:25 +0100

    abstract class

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,15 @@ import { CharactersCollection } from './sorter/helper/CharactersCollection';
 import { LinkedList } from './sorter/helper/LinkedList';
 
 // const numberscollection = new NumbersCollection([10, 28, 3, 4, -5]);
-// const sorter = new Sorter(numberscollection);
 
-// sorter.sort();
-// //console.log(numberscollection.data);
+// numberscollection.sort();
+// console.log(numberscollection.data);
 
 // const characterscoll = new CharactersCollection('danka je danka');
-// const sorter = new Sorter(characterscoll);
 
-// sorter.sort();
+// characterscoll.sort();
 
-//console.log(characterscoll.data);
+// console.log(characterscoll.data);
 
 const linkedlist = new LinkedList();
 
@@ -24,6 +22,5 @@ linkedlist.add(-50);
 linkedlist.add(5);
 linkedlist.add(-500);
 
-const sorter = new Sorter(linkedlist);
-sorter.sort();
+linkedlist.sort();
 linkedlist.print();
